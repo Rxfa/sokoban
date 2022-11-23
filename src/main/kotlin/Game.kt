322f2a28e825f655c2ed6d1226f1af355efefc9d
rxfa <rafa3rnrn@gmail.com>
2022-11-23 16:41:41 +0000

    Task 3 completed

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,4 +1,5 @@
 import pt.isel.canvas.*
+import Maze.*
 
 const val GRID_HEIGHT = 520
 const val GRID_WIDTH = 400
@@ -10,9 +11,32 @@ data class Man(val pos:Position, val dir:Direction, val push:Boolean = false)
 
 enum class Direction {UP, RIGHT, DOWN, LEFT}
 
-fun Game.draw(canvas: Canvas){
+fun Game.draw(canvas: Canvas, maze: Maze){
     canvas.erase()
-    man.drawMan(canvas)
+    //man.drawMan(canvas)
+    println(maze.cells)
+    maze.cells.drawMap(canvas, this.man)
+}
+
+fun List<Cell>.drawMap(canvas: Canvas, man: Man){
+    this.forEach {
+        val filename = when (it.type) {
+            Type.WALL -> "soko|40,217,40,52"
+            Type.BOX -> "soko|80,217,40,52"
+            Type.TARGET -> "soko|0,217,40,52"
+            Type.MAN -> when(man.dir){
+                Direction.DOWN -> "soko|40,104,40,52"
+                Direction.RIGHT -> "soko|40,52,40,52"
+                Direction.LEFT -> "soko|40,156,40,52"
+                Direction.UP -> "soko|40,0,40,52"
+            }
+        }
+        if (it.type == Type.MAN)
+            canvas.drawImage(filename, man.pos.col, man.pos.line, 40, 52)
+        else
+            canvas.drawImage(filename, it.pos.col * 40, it.pos.line * 52, 40, 52)
+
+    }
 }
 /*
     canvas.drawImage("soko|120,217,40,52",x,x,x,x) // desenhar caixa escura
@@ -54,15 +78,23 @@ fun Man.move(key: Int):Man {
     }
 }
 
+fun Man.isInLimits(mp: List<Cell>):Boolean{
+    val x=mp.map{it.pos} as List<Position>//listOf<Position>()
+    return x.contains(this.pos)
+
+} //map.map{it.pos}.contains(this.pos)
+
 fun main(){
     onStart {
         val gridDimension = Dimension(GRID_WIDTH, GRID_HEIGHT)
-        val arena = Canvas(gridDimension.width, gridDimension.height, WHITE)
-        var myGame = Game(gridDimension, Man(Position(50, 80), Direction.DOWN))
-        myGame.draw(arena)
+        val mazeDesc = loadMap(level1)
+        val arena = Canvas(mazeDesc.width * 40, mazeDesc.height * 52, WHITE)
+        val manCellPos = mazeDesc.cells.filter { it.type == Type.MAN}.first().pos
+        var myGame = Game(gridDimension, Man(Position(manCellPos.col * 40, manCellPos.line * 52), Direction.DOWN))
+        myGame.draw(arena, mazeDesc)
         arena.onKeyPressed{k ->
             myGame = Game(gridDimension, myGame.man.move(k.code))
-            myGame.draw(arena)
+            myGame.draw(arena, mazeDesc)
             }
         }
     onFinish {  }
